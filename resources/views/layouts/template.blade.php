ed4257841ba36c5e2d089dd4c68e0a8da2a641e1
Jason Lin <jason.lin.6086@gmail.com>
2017-06-13 11:54:10 -0700

    update default user icon

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -84,7 +84,11 @@
                                         @endif
                                         <li class="dropdown">
                                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                                <img src="{{ url('/').'/storage/'.Auth::user()->image }}" width="30" height="30" alt="" data-retina="true" style="border-radius:50%;margin-right:20px;" data-toggle="dropdown" >
+@php
+    $usericon = Auth::user()->image;
+    $usericon = is_null($usericon) ? "/image/defaultusericon.png" : "/storage/".$usericon;
+@endphp
+                                                <img src="{{ url('/').$usericon }}" width="30" height="30" alt="" data-retina="true" style="border-radius:50%;margin-right:20px;" data-toggle="dropdown" >
                                                 {{ Auth::user()->name }} <span class="caret"></span>
                                             </a>
 
